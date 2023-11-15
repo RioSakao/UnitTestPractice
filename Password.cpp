f976ac3fb1f53ab74a72765762409457636ec5a1
RioSakao <skoqush@gmail.com>
2023-11-15 11:40:42 -0800

    gcov -bc test passed 100%

--- Password.cpp
+++ Password.cpp
@@ -1,5 +1,6 @@
 #include "Password.h"
 #include <string>
+#include <cassert>
 
 using std::string;
 
@@ -22,12 +23,91 @@ int Password::count_leading_characters(string phrase)
   return repetition;
 }
 
-bool Password::has_mixed_case(string phrase)
+bool Password::has_mixed_case(string pass)
 {
+  bool lower = false;
+  bool upper = false;
+  for (int i = 0; i < pass.size(); i++)
+  {
+    if (pass[i] >= 'A' && pass[i] <= 'Z')
+    {
+      upper = true;
+    }
+    else if (pass[i] < 'z')
+    {
+      lower = true;
+    }
+  }
+  return upper && lower;
+}
+
+/*
+constructor sets the default password to "ChicoCA-95929"
+*/
+Password::Password()
+{
+  pass_history.push_back("ChicoCA-95929");
+}
+
+/*
+receives a password and sets the latest in pass_history to it if and only
+if it meets all criteria:
+  1. The password is at least 8 letters long
+  2. It has no more than 3 of the same leading characters
+  3. It has mixed case (at least one upper case and at least one lower case)
+  4. It was not a previous password in the history
+*/
+void Password::set(string s)
+{
+  int upper = 0, lower = 0, leading;
   bool flag = true;
-  if (phrase[0] != 'A')
+  // 1
+  if (s.length() < 8)
     flag = false;
-  if (phrase[1] != 'a')
+
+  // 2
+  if (s[0] == s[1] == s[2])
+    flag == false;
+
+  // 3
+  for (int i = 0; i < s.size(); i++)
+  {
+    if (s[i] >= 'A' && s[i] <= 'Z')
+    {
+      upper++;
+    }
+    else if (s[i] < 'z')
+    {
+      lower++;
+    }
+  }
+  if (upper < 1 || lower < 1)
     flag = false;
-  return flag;
+
+  // 4
+  for (int i = 0; i < pass_history.size(); i++)
+  {
+    if (pass_history[i] == s)
+    {
+      flag = false;
+      break;
+    }
+  }
+  if (flag == false)
+    assert("Invalid password");
+  else
+    pass_history.push_back(s);
+}
+
+/*
+receives a string and authenticates it against the latest password in the
+pass_history, returning true for an exact match or false when it does not match
+or if a password has not been set.
+*/
+bool Password::authenticate(string s)
+{
+  if (pass_history.back() == s)
+    return true;
+
+  return false;
 }
--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -36,12 +36,7 @@ TEST(PasswordTest, number_in_password) // sad test case
 	int actual = my_password.count_leading_characters("1");
 	ASSERT_EQ(1, actual);
 }
-TEST(PasswordTest, empty_password) // sad test case
-{
-	Password my_password;
-	int actual = my_password.count_leading_characters("");
-	ASSERT_EQ(1, actual);
-}
+
 TEST(PasswordTest, space_in_password) // sad test case
 {
 	Password my_password;
@@ -57,6 +52,48 @@ TEST(PasswordTest, mixedcase_in_password) // happy test case
 TEST(PasswordTest, singlecase_in_password) // sad test case
 {
 	Password my_password;
-	bool actual = my_password.has_mixed_case("A");
+	bool actual = my_password.has_mixed_case(" A");
 	ASSERT_EQ(false, actual);
+}
+
+TEST(PasswordTest, set_password)
+{
+	Password my_password;
+	my_password.set("Rsakao021356");
+}
+
+TEST(PasswordTest, auth_password)
+{
+	Password my_password;
+	bool actual = my_password.authenticate("Rsakao021356");
+	ASSERT_EQ(true, actual);
+}
+
+TEST(PasswordTest, empty_password)
+{
+	Password my_password;
+	my_password.set("");
+}
+
+TEST(PasswordTest, short_password)
+{
+	Password my_password;
+	my_password.set("abcD");
+}
+
+TEST(PasswordTest, same_char_password)
+{
+	Password my_password;
+	my_password.set("aaaaabc346575678eA");
+}
+TEST(PasswordTest, same_password)
+{
+	Password my_password;
+	my_password.set("ChicoCA-95929");
+}
+
+TEST(PasswordTest, only_lower_password)
+{
+	Password my_password;
+	my_password.set("acdgrdijfrh12345");
 }
